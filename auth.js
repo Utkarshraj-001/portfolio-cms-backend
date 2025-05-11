78bb241db705e76dae556623bf351b4a0725df49
Utkarsh Raj <abillionair01@gmail.com>
2025-05-11 18:16:38 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === "admin@1111.com" && password === "123456") {
+    res.status(200).json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+});
+
+module.exports = router;
